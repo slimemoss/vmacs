f8c80c3daae046dda471e2b68eacf83c80f0c6bd
slimemoss <common.tken@gmail.com>
2025-03-01 17:17:23 +0000

    起動時間の短縮

--- .emacs.d/conf/60_conffile.el
+++ .emacs.d/conf/60_conffile.el
@@ -1,16 +1,21 @@
 (use-package dockerfile-mode
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package docker-compose-mode
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package k8s-mode
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package json-mode
   :ensure t
+  :defer t
   :custom
   (js-indent-level 2))
 
 (use-package yaml-mode
-  :ensure t)
+  :ensure t
+  :defer t)
