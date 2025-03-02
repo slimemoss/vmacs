d7c29018c841bf9b76d4f292bd9d2a32bebc2ef6
slimemoss <common.tken@gmail.com>
2025-03-02 21:44:04 +0000

    リファクタ

--- vmacs.sh
+++ vmacs.sh
@@ -1,15 +1,52 @@
-CURDIR=$(cd $(dirname $0);pwd)
-if [ "$#" -ne 1 ]; then
-    WORKDIR=$(cd ~/work && pwd)
-else
-    WORKDIR=$(cd "$1" && pwd)
-fi
+#!/bin/bash
+
 BASEDIR=$(cd $(/usr/bin/dirname $0); pwd)
 IMAGE=vmacs
 CHOME=/root
 
-docker build --target builder -t $IMAGE-builder $BASEDIR
-docker build -t $IMAGE $BASEDIR
+usage() {
+    echo "Usage: $0 [OPTIONS] [WORKDIR]"
+    echo "  If WORKDIR is not specified, defaults to ~/work."
+    echo "Options:"
+    echo "  --help      Show this help message and exit."
+    echo "  --build     Build the Docker image before running."
+    exit 0
+}
+
+BUILD=false
+
+while [[ "$1" == --* ]]; do
+    case "$1" in
+        --help) usage ;;
+        --build) BUILD=true ;;
+        *) echo "Error: Unknown option: $1" >&2; usage; exit 1 ;;
+    esac
+    shift
+ done
+
+if [ "$#" -gt 1 ]; then
+    echo "Error: Too many arguments." >&2
+    usage
+    exit 1
+fi
+
+if [ "$#" -eq 1 ]; then
+    WORKDIR="$1"
+else
+    WORKDIR=~/work
+fi
+
+if [ ! -d "$WORKDIR" ]; then
+    echo "Error: Work directory '$WORKDIR' does not exist or is not a directory." >&2
+    exit 1
+fi
+
+WORKDIR=$(cd "$WORKDIR" && pwd)
+
+if [ "$BUILD" = true ]; then
+    docker build --target builder -t $IMAGE-builder $BASEDIR
+    docker build -t $IMAGE $BASEDIR
+fi
 
 docker run -it --rm \
        -v $HOME/.gitconfig:$CHOME/.gitconfig \
